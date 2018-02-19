b8bf9516291f38f281cefade80ad2287c8794e2b
Marlon Alagoda <marlon.alagoda@senacor.com>
2018-02-19 22:43:21 +0100

    pass state to app in frontend

--- src/ui/login/browser.js
+++ src/ui/login/browser.js
@@ -2,4 +2,4 @@ import React from 'react'
 import { hydrate } from 'react-dom'
 import App from './index'
 
-hydrate(<App />, document.getElementById('loginRoot'))
+hydrate(<App {...window.__APP_INITIAL_USER_STATE__} />, document.getElementById('loginRoot'))
