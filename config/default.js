45cfff62b1f2ca9863160443f23ee4748699132a
Marlon Alagoda <marlon.alagoda@senacor.com>
2018-02-20 01:17:26 +0100

    Fixed api connection string

--- config/default.js
+++ config/default.js
@@ -1,7 +1,7 @@
 const config = {
   port: 3000,
   staticFilesConnectionString: 'http://user-mf.us-east-1.elasticbeanstalk.com',
-  apiConnectionString: 'http://user-mf.us-east-1.elasticbeanstalk.com',
+  apiConnectionString: 'http://skipper.us-east-1.elasticbeanstalk.com',
   accessControlHeader: 'http://skipper.us-east-1.elasticbeanstalk.com'
 }
 
